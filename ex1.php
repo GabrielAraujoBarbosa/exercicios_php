99b81e2d81c48f6de4cd7c51971e234701379d91
Gabrielll <ogabriel.araujo2@gmail.com>
2026-03-24 21:43:37 -0300

    finalizando ex1 (80% finalizado)

--- ex1.php
+++ ex1.php
@@ -6,27 +6,103 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $mediaAluno1 = ( ($_POST["aluno1_nota1"] ?? 0) + ($_POST["aluno1_nota2"] ?? 0) ) / 2;
+
+            $mediaAluno2 = ( ($_POST["aluno2_nota1"] ?? 0) + ($_POST["aluno2_nota2"] ?? 0) ) / 2;
+
+            $mediaAluno3 = ( ($_POST["aluno3_nota1"] ?? 0) + ($_POST["aluno3_nota2"] ?? 0) ) / 2;
+
+            $mediaAluno4 = ( ($_POST["aluno4_nota1"] ?? 0) + ($_POST["aluno4_nota2"] ?? 0) ) / 2;
+
+            $mediaAluno5 = ( ($_POST["aluno5_nota1"] ?? 0) + ($_POST["aluno5_nota2"] ?? 0) ) / 2;
+
+            $mediaAluno6 = ( ($_POST["aluno6_nota1"] ?? 0) + ($_POST["aluno6_nota2"] ?? 0) ) / 2;
+        }
+    ?>
+
     <form action="" method="post">
         
         <fieldset>
-            <label for="numero1">1º Número:</label>
-            <input type="number" name="numero1" id="numero1">
+            <legend>Aluno 1</legend>
+            <label for="aluno1_nota1">1º Nota:</label>
+            <input type="number"  name="aluno1_nota1" id="aluno1_nota1" required>
+
+            <label for="aluno1_nota2">2° Nota:</label>
+            <input type="number" name="aluno1_nota2" id="aluno1_nota2" required>
+
+            <br>
+            <span>Média : <?php echo $mediaAluno1 ?? ''; ?></span>
             
-            <label for="numero2">2° Número:</label>
-            <input type="number" name="numero2" id="numero2">
+            <br>
+            <span></span>
+        </fieldset>
+
+        <fieldset>
+            <legend>Aluno 2</legend>
+            <label for="aluno2_nota1">1º Nota:</label>
+            <input type="number" name="aluno2_nota1" id="aluno2_nota1" required>
+            
+            <label for="aluno2_nota2">2° Nota:</label>
+            <input type="number" name="aluno2_nota2" id="aluno2_nota2" required>
+
+            <br>
+            <span>Média : <?php echo $mediaAluno2 ?? ''; ?></span>
+        </fieldset>
+
+        <fieldset>
+            <legend>Aluno 3</legend>
+            <label for="aluno3_nota1">1º Nota:</label>
+            <input type="number" name="aluno3_nota1" id="aluno3_nota1" required>
+            
+            <label for="aluno3_nota2">2° Nota:</label>
+            <input type="number" name="aluno3_nota2" id="aluno3_nota2" required>
+
+            <br>
+            <span>Média : <?php echo $mediaAluno3 ?? ''; ?></span>
+        </fieldset>
+
+        <fieldset>
+            <legend>Aluno 4</legend>
+            <label for="aluno4_nota1">1º Nota:</label>
+            <input type="number" name="aluno4_nota1" id="aluno4_nota1" required>
+            
+            <label for="aluno4_nota2">2° Nota:</label>
+            <input type="number" name="aluno4_nota2" id="aluno4_nota2" required>
+
+            <br>
+            <span>Média : <?php echo $mediaAluno4 ?? ''; ?></span>
+        </fieldset>
+
+        <fieldset>
+            <legend>Aluno 5</legend>
+            <label for="aluno5_nota1">1º Nota:</label>
+            <input type="number" name="aluno5_nota1" id="aluno5_nota1" required>
+            
+            <label for="aluno5_nota2">2° Nota:</label>
+            <input type="number" name="aluno5_nota2" id="aluno5_nota2" required>
+            
+            <br>
+            <span>Média : <?php echo $mediaAluno5 ?? ''; ?></span>
+        </fieldset>
+
+        <fieldset>
+            <legend>Aluno 6</legend>
+            <label for="aluno6_nota1">1º Nota:</label>
+            <input type="number" name="aluno6_nota1" id="aluno6_nota1" required>
+            
+            <label for="aluno6_nota2">2° Nota:</label>
+            <input type="number" name="aluno6_nota2" id="aluno6_nota2" required>
+            
+            <br>
+            <span>Média : <?php echo $mediaAluno6 ?? ''; ?></span>
         </fieldset>
 
         <button type="submit">Enviar</button>
     </form>
 
-    <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $numero1 = $_POST["numero1"];
-            $numero2 = $_POST["numero2"];
-
-            $media = ($numero1 + $numero2) / 2;
-        }
-    ?>
+    
 
 </body>
 </html>
